55015082fb9f74dd01a2b9769760467f03b4476f
pbetti-sie <88213850+pbetti-sie@users.noreply.github.com>
2024-03-25 08:55:23 +0000

    w4 project

--- w4/logger_config.py
+++ w4/logger_config.py
@@ -18,32 +18,35 @@ class Logger:
         self.f_handler = logging.FileHandler(os.path.join(CURRENT_FOLDER_NAME, 'logs', log_file_name))
 
         # Create formatters and add it to handlers
-        ######################################## YOUR CODE HERE ##################################################
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
         # set the logging formatter to the f_handler
-        ######################################## YOUR CODE HERE ##################################################
+        self.f_handler.setFormatter(formatter)
 
         ######################################## YOUR CODE HERE ##################################################
         # Add handlers to the logger and setlevel to DEBUG
+        self.logger.addHandler(self.f_handler)
+        self.logger.setLevel(logging.DEBUG)
         ######################################## YOUR CODE HERE ##################################################
 
     def warning(self, msg):
-        pass
         ######################################## YOUR CODE HERE ##################################################
+        self.logger.warning(msg)
         ######################################## YOUR CODE HERE ##################################################
 
     def error(self, msg):
-        pass
         ######################################## YOUR CODE HERE ##################################################
+        self.logger.error(msg)
         ######################################## YOUR CODE HERE ##################################################
 
     def info(self, msg):
-        pass
         ######################################## YOUR CODE HERE ##################################################
+        self.logger.info(msg)
         ######################################## YOUR CODE HERE ##################################################
 
     def debug(self, msg):
-        pass
         ######################################## YOUR CODE HERE ##################################################
+        self.logger.debug(msg)
         ######################################## YOUR CODE HERE ##################################################
 
 
